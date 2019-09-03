6d0d492cda5f13d8c1cfea7a52cc237e069f50a8
Jose Chavira <joeychavira2016@gmail.com>
2019-09-03 19:50:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,11 +33,12 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  response = gets.chomp.strip
-  if songs.include? {|song,index|
-    puts "Playing #{songs}"
-  }
-  else
-    puts "Invalid input, please try again"
-  end
-end
+   response = gets.chomp.strip
+   if response.to_i >= 1 && response.to_i <= songs.length
+     puts "Playing #{songs[response.to_i-1]}"
+   elsif songs.include?(response)
+     puts "Playing #{songs.find{|song| song == response}}"
+   else
+     puts "Invalid input, please try again"
+   end
+ end
